6ab82df30e4ab1d34da82622514f2acd41a1885b
fotuneb <fotuneb2334@gmail.com>
2024-10-27 01:51:49 +0300

    Добавление ролей

--- backend/app/create_admin.py
+++ backend/app/create_admin.py
@@ -1,25 +1,27 @@
 import asyncio
-from passlib.hash import bcrypt
-from app.user.models_user import UserModel
 from tortoise import Tortoise
+from user.models_user import UserModel
+from config import settings
+from passlib.hash import bcrypt
 
 async def create_admin():
+    postgres_url = "postgres://{username}:{password}@{host}:{port}/{dbname}".format(
+        username=settings.postgres_username,
+        password=settings.postgres_password,
+        host=settings.postgres_host,
+        port=settings.postgres_port,
+        dbname=settings.postgres_dbname,
+    )
+
     await Tortoise.init(
-        db_url="postgres://{username}:{password}@{host}:{port}/{dbname}".format(
-            username="your_username",
-            password="your_password",
-            host="localhost",
-            port=5432,
-            dbname="your_dbname"
-        ),
-        modules={"models": ["app.user.models_user"]}
+        db_url=postgres_url,
+        modules={"models": ["user.models_user"]}
     )
     await Tortoise.generate_schemas()
 
     password = "admin"  # Задайте пароль для администратора
     hashed_password = bcrypt.hash(password)
     
-    # Создаём пользователя с ролью "admin"
     admin_user = await UserModel.create(
         fullname="Admin User",
         login="admin",
@@ -30,5 +32,4 @@ async def create_admin():
     print("Admin user created:", admin_user.fullname, admin_user.login, admin_user.role)
     await Tortoise.close_connections()
 
-# Запускаем асинхронный скрипт
 asyncio.run(create_admin())
--- backend/app/user/routes_user.py
+++ backend/app/user/routes_user.py
@@ -10,48 +10,60 @@ from app.user.schemas_user import User, UserIn
 router1 = APIRouter()
 admin_router = APIRouter()
 
-@admin_router.post("/change-role/{user_id}")
+@admin_router.post("/api/change-role-admin/{user_id}")
 async def change_role(user_id: int, new_role: str, admin_user: UserModel = Depends(get_admin_user)):
     user = await UserModel.get(id=user_id)
     user.role = new_role
     await user.save()
     return {"msg": "Role updated successfully"}
 
-@admin_router.post("/change-fullname/{user_id}")
+@admin_router.post("/api/change-fullname-admin/{user_id}")
 async def change_fullname(user_id: int, new_fullname: str, admin_user: UserModel = Depends(get_admin_user)):
     user = await UserModel.get(id=user_id)
     if not re.fullmatch(r"[A-Za-zА-Яа-яёЁ\s\-]+", new_fullname):
-        raise ValueError("Fullname должен содержать только буквы, пробелы и дефисы.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Fullname должен содержать только буквы, пробелы и дефисы.",
+        )
     user.fullname = new_fullname
     await user.save()
     return {"msg": "Role updated successfully"}
 
-@admin_router.post("/change-password/{user_id}")
+@admin_router.post("/api/change-password-admin/{user_id}")
 async def change_password(user_id: int, new_password: str, admin_user: UserModel = Depends(get_admin_user)):
     user = await UserModel.get(id=user_id)
     if len(new_password) > 20:
-            raise ValueError("Пароль должен быть не длиннее 20 символов.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пароль должен быть не длиннее 20 символов.",
+        )
     user.password = bcrypt.hash(new_password)
     await user.save()
     return {"msg": "Role updated successfully"}
 
-@router1.get("/get_users")
+@router1.get("/api/get_users")
 async def get_users():
     users = await UserModel.all().values("id", "fullname", "role")
     return users
 
-@router1.get("/get_users/{user_id}")
-async def get_users(user_id: int):  
+@router1.get("/api/get_user/{user_id}")
+async def get_user(user_id: int):  
     user = await UserModel.get(id=user_id).values("fullname", "role", "about")
     return user
 
-@router1.post("/users")
+@router1.post("/api/users")
 async def create_user(user_in: UserIn):
     
     if not re.fullmatch(r"[A-Za-zА-Яа-яёЁ\s\-]+", user_in.fullname):
-            raise ValueError("Fullname должен содержать только буквы, пробелы и дефисы.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Fullname должен содержать только буквы, пробелы и дефисы.",
+        )
     if len(user_in.password1) > 20:
-            raise ValueError("Пароль должен быть не длиннее 20 символов.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пароль должен быть не длиннее 20 символов.",
+        )
     user = await UserModel.create(
         fullname = user_in.fullname, login=user_in.login, password=bcrypt.hash(user_in.password1)
     )
@@ -59,14 +71,19 @@ async def create_user(user_in: UserIn):
 
     return {"access_token": await create_token(user)}
 
-@router1.post("/change-about/{user_id}")
-async def change_role(user_id: int, new_about: str):
+@router1.post("/api/change-about/{user_id}")
+async def change_about(user_id: int, new_about: str):
     user = await UserModel.get(id=user_id)
+    if user.role == "guest":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "Нет прав",
+        )
     user.about = new_about
     await user.save()
     return {"msg": "About updated successfully"}
 
-@router1.post("/token")
+@router1.post("/api/token")
 async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = await authenticate_user(form_data.username, form_data.password)
 
