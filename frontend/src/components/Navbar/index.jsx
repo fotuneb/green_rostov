867a2d381b00b83b7bb13d0d6a5745d9fab57e19
Максим <m.zaharenkov@mail.ru>
2024-10-26 18:19:00 +0300

    Заглушка для редактирования инфо о себе

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,34 +1,45 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import Logout from "../Logout";
+import { UserProfileModal } from "../UserProfileModal"
 import "./navbar.css";
 
 function Navbar(props) {
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
   return (
-    <nav className="navbar">
-      <Link to="/">
-        <button className="nav-button">Kaban</button>
-      </Link>
-      {!props.token ? (
-        <div className="font-inter">
-          <Link to="/login">
-            <button className="nav-button">Вход</button>
-          </Link>
-          <Link to="/signup">
-            <button className="nav-button">Регистрация</button>
-          </Link>
-        </div>
-      ) : (
-        <div>
-          <Link to="/board">
-            <button className="nav-button">Моя доска</button>
-          </Link>
-          <span className="h-8 w-px mx-6 bg-gray-200" aria-hidden="true" />
-          <Logout setToken={props.setToken} />
-        </div>
-      )
-      }
-    </nav >
+    <>
+      <UserProfileModal isOpen={isModalOpen} onClose={closeModal} />
+      <nav className="navbar">
+        <Link to="/">
+          <button className="nav-button">Kaban</button>
+        </Link>
+        {!props.token ? (
+          <div className="font-inter">
+            <Link to="/login">
+              <button className="nav-button">Вход</button>
+            </Link>
+            <Link to="/signup">
+              <button className="nav-button">Регистрация</button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/board">
+              <button className="nav-button">Моя доска</button>
+            </Link>
+            <button onClick={openModal} className="nav-button">Профиль</button>
+            <span className="h-8 w-px mx-6 bg-gray-200" aria-hidden="true" />
+            <Logout setToken={props.setToken} />
+          </div>
+        )
+        }
+      </nav >
+    </>
   );
 }
 
---
+++ frontend/src/components/UserProfileModal/index.jsx
@@ -0,0 +1,113 @@
+
+import React, { useState } from 'react';
+import "./user_profile_modal.css"
+
+const EditProfile = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: '',
+        about: '',
+    });
+    const [passwords, setPasswords] = useState({
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+    });
+
+    const handleUserInfoChange = (e) => {
+        const { name, value } = e.target;
+        setUserInfo((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handlePasswordChange = (e) => {
+        const { name, value } = e.target;
+        setPasswords((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Логика отправки данных на сервер
+        console.log('Информация пользователя:', userInfo);
+        console.log('Пароли:', passwords);
+    };
+
+    return (
+        <div className="font-inter">
+            <h1 className="text-center">Редактировать профиль</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label className="user-profile-label">ФИО:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={userInfo.name}
+                        onChange={handleUserInfoChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label className="user-profile-label">
+                        О себе:
+                    </label>
+                    <input
+                        type="text"
+                        name="about"
+                        value={userInfo.about}
+                        onChange={handleUserInfoChange}
+                        required
+                    />
+                </div>
+                <h2 className="text-center">Изменение пароля</h2>
+                <div className="input-group">
+                    <label className="user-profile-label">
+                        Текущий пароль:
+                    </label>
+                    <input
+                        type="password"
+                        name="currentPassword"
+                        value={passwords.currentPassword}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label className="user-profile-label">
+                        Новый пароль:
+                    </label>
+                    <input
+                        type="password"
+                        name="newPassword"
+                        value={passwords.newPassword}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label className="user-profile-label">
+                        Подтвердите новый пароль:
+                    </label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        value={passwords.confirmPassword}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <button className="user-profile-save font-inter" type="submit">Сохранить изменения</button>
+            </form>
+        </div>
+    );
+};
+
+
+export const UserProfileModal = ({ isOpen, onClose }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content user-profile-modal" onClick={(e) => e.stopPropagation()}>
+                <EditProfile />
+            </div>
+        </div>
+    );
+};
