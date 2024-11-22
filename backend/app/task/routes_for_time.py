5d118267c9ac73e86015a942b806680544ba5c43
SeraphKAA <kizogyan.arman01@mail.ru>
2024-11-23 01:01:13 +0300

    Мини изменение в архитектуре тасок, добавление аватарок у пользователей

--- backend/app/task/routes_for_time.py
+++ backend/app/task/routes_for_time.py
@@ -3,51 +3,8 @@ from datetime import datetime, timezone, time
 from app.task.models import Task
 from app.task.routes import router
 from starlette.status import HTTP_400_BAD_REQUEST
+from app.task.util import process_deadline, format_time, datetime_to_seconds
 
-def process_deadline(input_deadline: str) -> datetime:
-    """
-    Преобразует строку дедлайна '31.12.2024 23:00:00' в объект datetime для сохранения в базе данных.
-    
-    :param input_deadline: Строка с датой и временем в формате 'дд.мм.гггг чч:мм:сс'.
-    :return: Объект datetime, совместимый с базой данных.
-    """
-    try:
-        # Преобразуем строку в объект datetime
-        deadline_datetime = datetime.strptime(input_deadline, "%d.%m.%Y %H:%M:%S")
-        return deadline_datetime
-    except ValueError as e:
-        raise ValueError(
-            f"Некорректный формат строки: '{input_deadline}'. "
-            f"Ожидается формат 'дд.мм.гггг чч:мм:сс', например: '31.12.2024 23:00:00'."
-        ) from e
-
-
-def format_time(seconds: int) -> datetime:
-    """Преобразует количество секунд в объект datetime.datetime с временем (HH:MM:SS)."""
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
-    # Создаём объект datetime с текущей датой и временем, соответствующим секундам
-    return datetime.combine(datetime.today(), time(hour=hours, minute=minutes, second=seconds))
-
-def datetime_to_seconds(dt: datetime) -> int:
-    """
-    Преобразует объект datetime в число секунд, прошедших с начала текущего года, месяца и дня.
-    Если datetime не содержит информации о временной зоне, считается локальным временем.
-    """
-    if dt is None:
-        return 0
-
-    # Получаем начало текущего дня
-    start_of_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=dt.tzinfo)
-
-    # Если dt не содержит информацию о временной зоне
-    if dt.tzinfo is None:
-        start_of_day = start_of_day.replace(tzinfo=timezone.utc)
-        dt = dt.replace(tzinfo=timezone.utc)
-
-    # Возвращаем разницу между текущей датой и dt в секундах
-    return int((dt - start_of_day).total_seconds())
 
 @router.post("/tasks/{task_id}/deadline")
 async def update_deadline(task_id: int, new_deadline: str):
