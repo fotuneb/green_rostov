592468407625bec9b5068bbad0c07046c1d83334
radon-dro <m.zaharenkov@dro.red>
2024-12-01 20:33:27 +0300

    Забыл вырезать принт

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -6,7 +6,6 @@ from aiohttp import web
 from app.handlers import router
 from app.http_handlers import send_changing, send_deadline, generate_link
 
-print(os.environ)
 
 async def main():
     bot = Bot(token=os.environ['BOT_SECRET_TOKEN'])
