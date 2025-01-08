07a810d53c90fa6427bb0fa2db9f7ed8a084a482
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-01-08 14:31:58 +0300

    Переименование src/index.ts в src/index.tsx для корректного рендера App

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
